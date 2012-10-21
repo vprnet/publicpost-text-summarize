031addc1a6f9c71d4bbdf5f31773e4f6bd81909b
Matt MacDonald <matt.macdonald@gmail.com>
2012-10-21 18:51:26 -0400

    using httpclient instead of yomu directly

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'http://rubygems.org'
+gem 'httpclient'
 gem 'foreman'
 gem 'rack'
 gem 'sinatra'
--- app.rb
+++ app.rb
@@ -1,9 +1,20 @@
+require 'httpclient'
 require 'sinatra'
 require 'yomu'
 require 'open-uri'
 
+@@http_client = HTTPClient.new
+@@http_client.connect_timeout = 60
+
 get '/extract?*' do
   url = URI::encode(params[:url])
-  yomu = Yomu.new url
-  yomu.text
+  response = @@http_client.get(url, :follow_redirect => true)
+  Timeout::timeout(60) {
+    text = safe_squeeze(Yomu.read(:text, response.body))
+  }
+end
+
+# Remove superfluous whitespaces from the given string
+def safe_squeeze(value)
+  value = value.strip.gsub(/\s+/, ' ').squeeze(' ').strip unless value.nil?
 end
