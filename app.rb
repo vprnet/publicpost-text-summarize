58ae6ae37fd2c7f36de4e7526637a7ee57b0dfcd
Matt MacDonald <matt.macdonald@gmail.com>
2012-11-05 20:03:11 -0500

    topics

--- app.rb
+++ app.rb
@@ -13,3 +13,9 @@ get '/summarize?*' do
   summed_text = text.summarize(:ratio => ratio)
   return "#{summed_text}"
 end
+
+get '/topics?*' do
+  text = params[:text]
+  topic_text = text.summarize(:topics => true)
+  return "#{topic_text[1]}"
+end
